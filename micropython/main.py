0a1a6b8d94bee17b1364d69b1a6ee56fc823adce
Robin Puthli <r.puthli@itude.com>
2017-08-15 18:40:58 +0200

    Working test script

--- micropython/main.py
+++ micropython/main.py
@@ -15,39 +15,32 @@ import binascii
 import pycom
 import CoZIR
 import LEDColors
-import DeepSleep
+
+
+print('Starting up...')
+############################################################################
+#setup sensor
+coZIR = CoZIR.CoZIR()
+coZIR.setModeLowPower()
+coZIR.calibrateCO2()
 
 #stop the blue light from flickering
 pycom.heartbeat(False)
 
+############################################################################
+#setup LoRa connection
 connection = TTN.LoRaConnection()
-coZIR = CoZIR.CoZIR()
-sleep = DeepSleep.DeepSleep()
-print(sleep.get_wake_status())
-if (sleep.get_wake_status()["wake"] >> 5 == 1): # if power on bit set
-    print('Starting up...')
-    ############################################################################
-    # setup sensor
-    coZIR.setModeLowPower()
-    coZIR.calibrateCO2()
-
-    #setup LoRa connection
-    connection.start()
-    connection.lora.nvram_save()
-else:
-    print('Woken up after deep sleep timer expired')
-    connection.lora.nvram_restore() # restore saved LoRa connection
+connection.start()
 
 ############################################################################
-#main execution loop, triggered by awakening from deep sleep
+#main execution looop
 minutes = 1 # number of minutes to wait between polling the sensor and sending
 CO2warninglevel = 100
 CO2dangerlevel = 1400
 while 1:
     pycom.rgbled(LEDColors.color['purple'])
     coZIR.setModePolling()
-    print("Let sensor warm up...")
-    time.sleep(15) #let sensor warmup cyle finish
+    time.sleep(10) #let sensor warmup cyle finish
     co2 = coZIR.getCO2()
     hum = coZIR.getHumidity()
     temp = coZIR.getTemperature()
@@ -57,17 +50,13 @@ while 1:
     print(dataline)
     data = bytearray()
     for i in dataline:
-        try:
-            data.append(int(chr(i))) # convert ascii characters to bytes
-        except ValueError:
-            print("Non number in data - sensor is producing gibberish")
+        data.append(int(chr(i))) # convert ascii characters to bytes
     connection.sendData(data)
-    connection.lora.nvram_save()
 
-    # co2int = int(co2)
-    # if(co2int>CO2dangerlevel):
-    #     pycom.rgbled(LEDColors.color['red'])
-    # if(co2int>CO2warninglevel):
-    #     pycom.rgbled(LEDColors.color['orange'])
+    co2int = int(co2)
+    if(co2int>CO2dangerlevel):
+        pycom.rgbled(LEDColors.color['red'])
+    if(co2int>CO2warninglevel):
+        pycom.rgbled(LEDColors.color['orange'])
 
-# sleep.go_to_sleep(10)
+    time.sleep(60*minutes)
